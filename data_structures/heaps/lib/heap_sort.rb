57d84acbdb6afb41200107a0e4658993cdd16c1b
Yue Gu <yuegu1993@gmail.com>
2017-06-26 19:02:17 -0700

    heap sort in place solution

--- data_structures/heaps/lib/heap_sort.rb
+++ data_structures/heaps/lib/heap_sort.rb
@@ -2,5 +2,19 @@ require_relative "heap"
 
 class Array
   def heap_sort!
+    prc = Proc.new {|a, b| -1* (a <=> b)}
+    i = 1
+    while i < self.length
+      BinaryMinHeap.heapify_up(self, i, self.length, &prc)
+      i+=1
+    end
+
+    j = self.length - 1
+
+    while j > 0
+      self[j], self[0] = self[0], self[j]
+      BinaryMinHeap.heapify_down(self, 0, j, &prc)
+      j -= 1
+    end
   end
 end
